5421a4f5246688051953a231392561938eecabcc
jefferydecker <jefferydecker@gmail.com>
2020-09-15 20:20:59 -0500

    Initial commit

---
+++ format_output/average_scores.py
@@ -0,0 +1,11 @@
+"""
+Program: average_scores.py
+Author: Jeffery Decker jefferydecker@gmail.com jmdecker1@dmacc.edu
+Last Modified: ___
+
+The purpose of this program is to ___
+"""
+
+
+def average():
+    pass
